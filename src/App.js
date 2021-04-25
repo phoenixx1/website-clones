df99cbc10754d3dca296947968d35701d8e64026
phoenixx1 <xphoenix1010@gmail.com>
2021-04-25 17:45:58 +0530

    created-login-page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
